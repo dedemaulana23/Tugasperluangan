76ef70c458ae2f2b4818e5737fa5f272942b8573
dedemaulana23 <146941281+dedemaulana23@users.noreply.github.com>
2023-10-20 15:57:02 +0700

    Add files via upload

--- Tugasperluangan.java
+++ Tugasperluangan.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Tugasperluangan {
     public static boolean isPalindrome(String word) {
-        word = word.toLowerCase().replaceAll("\\s", ""); 
+        word = word.toLowerCase().replaceAll("\\s", "");
         int length = word.length();
         for (int i = 0; i < length / 2; i++) {
             if (word.charAt(i) != word.charAt(length - i - 1)) {
@@ -20,7 +22,28 @@ public class Tugasperluangan {
         System.out.println(kata2 + ": " + (isPalindrome(kata2) ? "palindrome" : "bukan palindrome"));
         System.out.println(kata3 + ": " + (isPalindrome(kata3) ? "palindrome" : "bukan palindrome"));
         System.out.println(kata4 + ": " + (isPalindrome(kata4) ? "palindrome" : "bukan palindrome"));
+
+        System.out.println();
+
+    int[] deretA = {1, 2, 4, 3, 4, 6, 5, 6, 8, 7};
+        for (int angka : deretA) {
+        System.out.println(angka);
+        }
+        System.out.println();
+
+    int[] deretB = {1, 2, 3, 6, 11, 20, 37, 68};
+        for (int angka : deretB) {
+        System.out.println(angka);
+       }
+        
+        System.out.println();
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 4; j++) {
+            int angka = i + (j - 1) * 3;
+            System.out.print(angka + " ");
+        }
+        System.out.println();
     }
 
-    
+    }   
 }
